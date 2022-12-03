19d2412a00792a4fc0c0afef606eb8e761abaaa0
Andres Morales <appandres993@gmail.com>
2022-12-03 18:20:45 -0500

    Se modifica el layout para /dasgboard

--- src/pages/Dashboart/dashboart.jsx
+++ src/pages/Dashboart/dashboart.jsx
@@ -5,59 +5,58 @@ import GraphUsers from "../../components/GraphUsers/graphUsers";
 import Header from "../../components/Header/header";
 import Navbar from "../../components/Navbar/navbar";
 import "./dashboart.css";
+import Layout from "../../components/Layout";
 
 export default function Dashboard() {
   return (
     <>
       <Header />
-      <div className="container-fluid divider-top">
-        <div className="row">
-          <div className="col-2 divider-right p-4">
-            <Navbar />
-          </div>
-          <div className="col-6 container-dos divider-right">
-            <div className="container-promedio background-color "></div>
-            <div className="container-acountUser">
-              <div className="background-color  container-element">
-                <div className="row">
-                  <div className="col-5">
-                    <img src="" alt="" />
-                    aca
-                  </div>
-                  <div className="col-5">aca</div>
-                  <div className="col-5">aca</div>
-                </div>
-                <span>Usuarios activos</span>
-              </div>
-              <div className="background-color  container-element">
-                <div className="row">
-                  <div className="col-5">
-                    <img src="" alt="" />
-                    imagen
-                  </div>
-                  <div className="col-5">aca</div>
-                  <div className="col-5">aca</div>
-                </div>
-                <span>Usuarios activos la ultima semana</span>
-              </div>
-            </div>
-            <div className="container-progres background-color "></div>
-            <div className="container-activate">
-              <span>
-                Da click aquí para comenzar el proceso de activar tus usuarios
-              </span>
-              <img src="https://iconos8.es/icon/368/subir" alt="" />
-            </div>
-          </div>
-          <div class="col-4 container-tres">
-            {" "}
+      <Layout
+        navBar={<Navbar />}
+        sideBar={
+          <>
             <Calendar />
             <AccountUser />
             <Categorias />
             <GraphUsers />
+          </>
+        }
+      >
+        <div className="">
+          <div className="container-promedio background-color "></div>
+          <div className="container-acountUser">
+            <div className="background-color  container-element">
+              <div className="row">
+                <div className="col-5">
+                  <img src="" alt="" />
+                  aca
+                </div>
+                <div className="col-5">aca</div>
+                <div className="col-5">aca</div>
+              </div>
+              <span>Usuarios activos</span>
+            </div>
+            <div className="background-color  container-element">
+              <div className="row">
+                <div className="col-5">
+                  <img src="" alt="" />
+                  imagen
+                </div>
+                <div className="col-5">aca</div>
+                <div className="col-5">aca</div>
+              </div>
+              <span>Usuarios activos la ultima semana</span>
+            </div>
+          </div>
+          <div className="container-progres background-color "></div>
+          <div className="container-activate">
+            <span>
+              Da click aquí para comenzar el proceso de activar tus usuarios
+            </span>
+            <img src="https://iconos8.es/icon/368/subir" alt="" />
           </div>
         </div>
-      </div>
+      </Layout>
     </>
   );
 }

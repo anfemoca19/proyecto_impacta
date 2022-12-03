c3963edbdeb47bcd2c638c39aaa6d54811b1a147
Andres Morales <appandres993@gmail.com>
2022-12-03 16:15:04 -0500

    Add Navbar styles

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,40 +1,36 @@
 import { Link } from "react-router-dom";
-import Icon_inicio from "../../imagenes/icono-menu-1-fondo-blanco.png";
-import Icon_config from "../../imagenes/icono-menu-2-fondo-blanco.png";
-import Icon_user from "../../imagenes/icono-menu-3-fondo-blanco.png";
-import Icon_cursos from "../../imagenes/icono-menu-5-fondo-blanco.png";
 import "../Navbar/navbar.css";
 
 export default function Navbar() {
   return (
-    <>
-      <nav class="navbar">
-        <Link class="navbar-brand" to="/dashboard">
-          <img className="icon-style" src={Icon_inicio} alt="icon-inicio" />
-          Inicio
+    <ul className="ps-0">
+      <li class="nav-item mb-2">
+        <Link class="nav-link fs-6" to="/dashboard">
+          <span className="nav-icon"></span>
+          <span>Inicio</span>
         </Link>
-      </nav>
+      </li>
 
-      <nav class="navbar ">
-        <Link class="navbar-brand mb-0 h1 link-style" to="/configuration">
-          <img className="icon-style" src={Icon_config} alt="icon-config" />
-          Configuración
+      <li class="nav-item mb-2">
+        <Link class="nav-link fs-6 mb-0 h1 link-style" to="/configuration">
+          <span className="nav-icon icon-config" />
+          <span>Configuración</span>
         </Link>
-      </nav>
+      </li>
 
-      <nav class="navbar ">
-        <Link class="navbar-brand link-style" to="/users">
-          <img className="icon-style" src={Icon_user} alt="icon-user" />
-          Usuarios
+      <li class="nav-item mb-2">
+        <Link class="nav-link fs-6 link-style" to="/users">
+          <span className="nav-icon icon-user" />
+          <span>Usuarios</span>
         </Link>
-      </nav>
+      </li>
 
-      <nav class="navbar ">
-        <Link class="navbar-brand mb-0 h1 link-style">
-          <img className="icon-style" src={Icon_cursos} alt="icon-play" />
-          Cursos
+      <li class="nav-item mb-2">
+        <Link class="nav-link fs-6 mb-0 h1 link-style">
+          <span className="nav-icon icon-course" />
+          <span>Cursos</span>
         </Link>
-      </nav>
-    </>
+      </li>
+    </ul>
   );
 }

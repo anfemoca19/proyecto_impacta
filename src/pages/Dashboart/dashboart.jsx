d4e893407973d59512ab95f7a8c42ea053faa00f
Andres Morales <appandres993@gmail.com>
2022-12-06 23:01:30 -0500

    Style container acountUser

--- src/pages/Dashboart/dashboart.jsx
+++ src/pages/Dashboart/dashboart.jsx
@@ -4,6 +4,11 @@ import Categorias from "../../components/Categories/categories";
 import GraphUsers from "../../components/GraphUsers/graphUsers";
 import Header from "../../components/Header/header";
 import Navbar from "../../components/Navbar/navbar";
+
+import upload from "../../imagenes/icono-carga-de-archivos.png";
+import usuarios_icon from "../../imagenes/icono-usuarios-activos.png";
+import violet_porcentaje from "../../imagenes/violet-porcentaje.png";
+
 import "./dashboart.css";
 import Layout from "../../components/Layout";
 
@@ -24,36 +29,63 @@ export default function Dashboard() {
       >
         <div className="">
           <div className="container-promedio background-color "></div>
-          <div className="container-acountUser">
-            <div className="background-color  container-element">
-              <div className="row">
-                <div className="col-5">
-                  <img src="" alt="" />
-                  aca
+          <div className="container-fluid d-flex justify-content-between  align-items-center">
+            <div className="background-color  col text-center rounded container-acountUser">
+              <div className="row ">
+                <div className="col-4 d-flex justify-content-center align-items-center">
+                  <img className="user-icon" src={usuarios_icon} alt="" />
                 </div>
-                <div className="col-5">aca</div>
-                <div className="col-5">aca</div>
+                <div className="col-4 d-flex justify-content-center align-items-center">
+                  <span className="style-number">70</span>
+                </div>
+                <div className="col-4 d-flex justify-content-center align-items-center">
+                  <span className="circle-violet"></span>
+                </div>
               </div>
-              <span>Usuarios activos</span>
+              <span className="style-text">Usuarios activos</span>
             </div>
-            <div className="background-color  container-element">
+            <div className="background-color  col text-center rounded container-acountUser">
               <div className="row">
-                <div className="col-5">
-                  <img src="" alt="" />
-                  imagen
+                <div className="col-4 d-flex justify-content-center align-items-center">
+                  <img className="user-icon" src={usuarios_icon} alt="" />
                 </div>
-                <div className="col-5">aca</div>
-                <div className="col-5">aca</div>
+                <div className="col-4 d-flex justify-content-center align-items-center">
+                  <span className="style-number">70</span>
+                </div>
+                <div className="col-4 d-flex justify-content-center align-items-center">
+                  <span className="circle-violet"></span>
+                </div>
               </div>
-              <span>Usuarios activos la ultima semana</span>
+              <span className="style-text">
+                Usuarios activos la ultima semana
+              </span>
             </div>
           </div>
-          <div className="container-progres background-color "></div>
-          <div className="container-activate">
+          <div className=" background-color mt-2">
+            <div>
+              <div>
+                <img src={violet_porcentaje} alt="" />
+              </div>
+              <div>
+                <img src={violet_porcentaje} alt="" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="row">
+                <span className="circle-violet"></span>
+                <span>Consumo promedio de contenido por usuario</span>
+              </div>
+              <div className="row ">
+                <span className="circle-green"></span>
+                <span> total de contenido consumido por la empresa</span>
+              </div>
+            </div>
+          </div>
+          <div className="container-activate d-flex justify-content-between align-items-center ps-4 pe-4">
             <span>
               Da click aquí para comenzar el proceso de activar tus usuarios
             </span>
-            <img src="https://iconos8.es/icon/368/subir" alt="" />
+            <img className="icon-upload" src={upload} alt="icon-upload" />
           </div>
         </div>
       </Layout>

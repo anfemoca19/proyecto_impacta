f3450765913495424a5c6099ff94a6ef8e8a6ce7
Andres Morales <appandres993@gmail.com>
2022-12-09 23:01:24 -0500

    Add icon section categories

--- src/components/Categories/categories.jsx
+++ src/components/Categories/categories.jsx
@@ -1,35 +1,85 @@
+import icon_nutricion_blanco from "../../imagenes/icono-categoria-nutricion-blanco.png";
+import icon_estilo_vida_blanco from "../../imagenes/icono-categoria-estilo-de-vida-blanco.png";
+import icon_actividad_fisica_blanco from "../../imagenes/icono-categoria-actividad-fisica-blanco.png";
+import icon_yoga_blanco from "../../imagenes/icono-categoria-yoga-blanco.png";
+import icon_inteligencia_blanco from "../../imagenes/icono-categoria-actividad-fisica-blanco.png";
+import icon_finanzas_blanco from "../../imagenes/icono-categoria-actividad-fisica-blanco.png";
+
+import "../Categories/categories.css";
+import { Link } from "react-router-dom";
+
 export default function Categorias() {
   return (
     <>
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <img src="..." alt="..." class="rounded" />
-            <h6 class="card-subtitle mb-2 text-muted">Nutricón</h6>
+      <div className="container  mb-5 mt-5">
+        <span className="title_style">Categorias</span>
+
+        <div className="row mt-3">
+          <div className="col text-center style-container container-icon">
+            <Link className="nav-link">
+              <span
+                src={icon_nutricion_blanco}
+                alt="icon_nutricion"
+                className="rounded style-icon icon-nutricion"
+              />
+              <h6 className="card-subtitle mb-2 text-muted">Nutricón</h6>
+            </Link>
           </div>
-          <div class="col">
-            <img src="..." alt="..." class="rounded" />
-            <h6 class="card-subtitle mb-2 text-muted">Estilo de vida</h6>
+          <div className="col text-cente container-icon">
+            <Link className="nav-link">
+              <span
+                src={icon_estilo_vida_blanco}
+                alt="icon_estilo_de_vida"
+                className="rounded style-icon icon_estilo_vida_blanco "
+              />
+              <h6 className="card-subtitle mb-2 text-muted">Estilo de vida</h6>
+            </Link>
           </div>
-          <div class="col">
-            <img src="..." alt="..." class="rounded" />
-            <h6 class="card-subtitle mb-2 text-muted">Actividad fisica</h6>
+          <div className="col text-center container-icon">
+            <Link className="nav-link">
+              <span
+                src={icon_actividad_fisica_blanco}
+                alt="icon_actividad_fisica"
+                className="rounded style-icon icon_actividad_fisica_blanco"
+              />
+              <h6 className="card-subtitle mb-2 text-muted">
+                Actividad fisica
+              </h6>
+            </Link>
           </div>
         </div>
-        <div class="row">
-          <div class="col">
-            <img src="..." alt="..." class="rounded" />
-            <h6 class="card-subtitle mb-2 text-muted">Yoga</h6>
+        <div className="row">
+          <div className="col text-center container-icon">
+            <Link className="nav-link" to="">
+              <span
+                src={icon_yoga_blanco}
+                alt="icon_yoga"
+                className="rounded style-icon icon_yoga_blanco"
+              />
+              <h6 className="card-subtitle mb-2 text-muted">Yoga</h6>
+            </Link>
           </div>
-          <div class="col">
-            <img src="..." alt="..." class="rounded" />
-            <h6 class="card-subtitle mb-2 text-muted">
-              Inteligencia emocional
-            </h6>
+          <div className="col text-center container-icon">
+            <Link className="nav-link" to="">
+              <span
+                src={icon_inteligencia_blanco}
+                alt="icon_inteligencia"
+                className="rounded style-icon icon_inteligencia_blanco"
+              />
+              <h6 className="card-subtitle mb-2 text-muted">
+                Inteligencia emocional
+              </h6>
+            </Link>
           </div>
-          <div class="col">
-            <img src="..." alt="..." class="rounded" />
-            <h6 class="card-subtitle mb-2 text-muted">Finanzas</h6>
+          <div className="col text-center container-icon">
+            <Link className="nav-link" to="">
+              <span
+                src={icon_finanzas_blanco}
+                alt="icon_finanzas"
+                className="rounded style-icon icon_finanzas_blanco"
+              />
+              <h6 className="card-subtitle mb-2 text-muted">Finanzas</h6>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/GraphUsers/graphUsers.jsx
+++ src/components/GraphUsers/graphUsers.jsx
@@ -1,7 +1,7 @@
 import "../GraphUsers/graphUsers.css";
 export default function GraphUsers() {
   return (
-    <div class="container-graph">
+    <div class="container d-flex justify-content-between align-items-center rounded container-account">
       {/* <div class="row">
         <div class="col container-elements">
           <img src="..." alt="icon-person" class="rounded" />

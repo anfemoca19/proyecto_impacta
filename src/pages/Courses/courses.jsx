31e2cde825b3784b4fa66c39500d93802ac98322
Andres Morales <appandres993@gmail.com>
2022-12-23 14:59:10 -0500

    Se agrega categorias a los cursos

--- src/pages/Courses/courses.jsx
+++ src/pages/Courses/courses.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import AccountUser from "../../components/AcountUser/accountUser";
 import Calendar from "../../components/Calendar/calendar";
 import Categorias from "../../components/Categories/categories";
@@ -21,7 +22,55 @@ export default function Courses() {
           </>
         }
       >
-        <div className=""></div>
+        <div className="container-fluid">
+          <span className="title-style">Categorias</span>
+          <div className="row mt-3">
+            <div className="col text-center style-container container-icon">
+              <Link className="nav-link">
+                <span className="rounded style-icon icon-nutricion-blanco" />
+                <h6 className="card-subtitle mb-2 text-muted">Nutricón</h6>
+              </Link>
+            </div>
+            <div className="col text-cente container-icon">
+              <Link className="nav-link">
+                <span className="rounded style-icon icon-estilo-vida-blanco " />
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Estilo de vida
+                </h6>
+              </Link>
+            </div>
+            <div className="col text-center container-icon">
+              <Link className="nav-link">
+                <span className="rounded style-icon icon-actividad-fisica-blanco" />
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Actividad fisica
+                </h6>
+              </Link>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col text-center container-icon">
+              <Link className="nav-link" to="">
+                <span className="rounded style-icon icon-yoga-blanco" />
+                <h6 className="card-subtitle mb-2 text-muted">Yoga</h6>
+              </Link>
+            </div>
+            <div className="col text-center container-icon">
+              <Link className="nav-link" to="">
+                <span className="rounded style-icon icon-inteligencia-blanco" />
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Inteligencia emocional
+                </h6>
+              </Link>
+            </div>
+            <div className="col text-center container-icon">
+              <Link className="nav-link" to="">
+                <span className="rounded style-icon icon-finanzas-blanco" />
+                <h6 className="card-subtitle mb-2 text-muted">Finanzas</h6>
+              </Link>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   );

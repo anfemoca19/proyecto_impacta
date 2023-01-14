fb7e169a2d1bd32621de7834e34956a625b4c264
Andres Morales <appandres993@gmail.com>
2023-01-14 18:42:49 -0500

    Se crea estilos mobile parael header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,29 +1,38 @@
 import styles from "./header.module.scss";
+import icon_hamburguer from "../../imagenes/icono-hamburguesa-gris.png";
 import Icon_search from "../../imagenes/icono-buscador.png";
 import Logo from "../../imagenes/logo-en-color.png";
 import Foto_perfil from "../../imagenes/foto-perfil.jpg";
+
 import Input from "../UI/Input/input";
 import clsx from "clsx";
 import { USER_PERMISSIONS } from "../../constants/user_const";
 
 export default function Header() {
   return (
-    <div className={clsx("container-fluid")}>
-      <div className={clsx("row")}>
+    <div className={clsx("container-fluid mt-3")}>
+      <div className={clsx("row", styles["container-web"])}>
         <div className={clsx("col-2 p-2", styles["container-logo"])}>
           <img className={clsx(styles.logo)} src={Logo} alt="logo" />
         </div>
-        <div className={clsx("col-6 p-2 d-flex align-items-center")}>
-          <div className={clsx(styles["container-data-user"])}>
-            <div className={clsx(styles["container-foto-perfil"])}>
-              <img
-                className={clsx(styles["foto-perfil"])}
-                src={Foto_perfil}
-                alt=""
-              />
+        <div
+          className={clsx(
+            "col-8 p-4 d-flex align-items-center",
+            styles["container-"]
+          )}
+        >
+          {!USER_PERMISSIONS.isAdmin && (
+            <div className={clsx(styles["container-data-user"])}>
+              <div className={clsx(styles["container-foto-perfil"])}>
+                <img
+                  className={clsx(styles["foto-perfil"])}
+                  src={Foto_perfil}
+                  alt=""
+                />
+              </div>
+              <span>Nombre Administrador / Nombre empresa</span>
             </div>
-            <span>Nombre Administrador / Nombre empresa</span>
-          </div>
+          )}
           {USER_PERMISSIONS.isAdmin && (
             <div className={clsx(styles["container-data-user"])}>
               <div className={clsx(styles["container-foto-perfil"])}>
@@ -38,6 +47,29 @@ export default function Header() {
           )}
         </div>
         <div
+          className={clsx("col-2 p-2 ", styles["container-pricipal-search"])}
+        >
+          <div className={clsx(styles["container-search"])}>
+            <img
+              className={clsx(styles["icon-search"])}
+              src={Icon_search}
+              alt="icon-sarch"
+            />
+            <Input
+              className={clsx("border-0 ", styles["input-search"])}
+              type="text"
+              placeholder="Buscar"
+            ></Input>
+          </div>
+        </div>
+      </div>
+
+      {/* mobile */}
+      <div className={clsx(styles["contianer-mobile"])}>
+        <div className={clsx("col-4 p-2", styles["container-logo"])}>
+          <img className={clsx(styles.logo)} src={Logo} alt="logo" />
+        </div>
+        <div
           className={clsx("col-4 p-2 ", styles["container-pricipal-search"])}
         >
           <div className={clsx(styles["container-search"])}>
@@ -47,11 +79,51 @@ export default function Header() {
               alt="icon-sarch"
             />
             <Input
-              className={clsx("border-0", styles["input-search-header"])}
+              className={clsx("border-0 ", styles["input-search"])}
               type="text"
               placeholder="Buscar"
             ></Input>
           </div>
+        </div>
+        <div
+          className={clsx("col-4 p-2 ", styles["container-menu-hamburguesa"])}
+        >
+          <img
+            src={icon_hamburguer}
+            alt="icon-hamburguer"
+            className={clsx(styles["icon-hamburger"])}
+          />
+        </div>
+      </div>
+      {/* tablet */}
+      <div className={clsx(styles["contianer-tablet"])}>
+        <div className={clsx("col-4 p-2", styles["container-logo"])}>
+          <img className={clsx(styles.logo)} src={Logo} alt="logo" />
+        </div>
+        <div
+          className={clsx("col-4 p-2 ", styles["container-pricipal-search"])}
+        >
+          <div className={clsx(styles["container-search"])}>
+            <img
+              className={clsx(styles["icon-search"])}
+              src={Icon_search}
+              alt="icon-sarch"
+            />
+            <Input
+              className={clsx("border-0 ", styles["input-search"])}
+              type="text"
+              placeholder="Buscar"
+            ></Input>
+          </div>
+        </div>
+        <div
+          className={clsx("col-4 p-2 ", styles["container-menu-hamburguesa"])}
+        >
+          <img
+            src={icon_hamburguer}
+            alt="icon-hamburguer"
+            className={clsx(styles["icon-hamburger"])}
+          />
         </div>
       </div>
     </div>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,8 +3,8 @@ const Layout = ({ navBar, children, sideBar }) => {
     <div className="container-fluid ">
       <div className="row">
         <div className="col-2 p-4 ">{navBar}</div>
-        <div className="col-6 p-4">{children}</div>
-        <div className="col-4 ">{sideBar}</div>
+        <div className="col-8 p-4">{children}</div>
+        <div className="col-2 ">{sideBar}</div>
       </div>
     </div>
   );

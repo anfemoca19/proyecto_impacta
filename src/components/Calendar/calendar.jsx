5f12b4335ea31098b4097137d53f35faf80897e7
Andres Morales <appandres993@gmail.com>
2023-01-16 22:42:32 -0500

    Se estamodificando estilos del calendario

--- src/components/Calendar/calendar.jsx
+++ src/components/Calendar/calendar.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import CalendarLibrary from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import "./Calendar.scss";
+import "./calendar.scss";
 
 export default function Calendar() {
   const [date, setDate] = useState(new Date());
   return (
-    <div className="container-fluid calendar mb-5 mt-4 rounded p-3">
+    <div
+      className="container-fluid calendar mb-5 mt-4 p-3"
+      // className={clsx("container-fluidmb-5 mt-4 p-3", styles["calendar"])}
+    >
       <CalendarLibrary
         onChange={setDate}
         value={date}
-        // nextLabel="month>>"
         nextAriaLabel="Ir al próximo mes"
         next2Label=""
         next2AriaLabel="Ir al próximo año"
-        // prevLabel="< <mes"
         prevAriaLabel="Ir al mes anterior"
         prev2Label=""
         prev2AriaLabel="Ir al año anterior"

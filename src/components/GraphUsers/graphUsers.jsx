8e59f9153dd971f1bfd79a0150eded449e9aa7c9
Andres Morales <appandres993@gmail.com>
2023-01-18 20:50:25 -0500

    stilos para la chart

--- src/components/GraphUsers/graphUsers.jsx
+++ src/components/GraphUsers/graphUsers.jsx
@@ -38,62 +38,60 @@ export default function GraphUsers() {
   return (
     <div
       className={clsx(
-        "container-fluid d-flex align-items-center mb-3 p-3",
+        "mt-5 container-fluid  align-items-center mb-3 p-3",
         styles["container-account-graph"]
       )}
     >
-      <div className={clsx("row")}>
-        <div className={clsx("d-flex mb-3")}>
-          <CircleColor className={clsx("me-2", "circle-blue-sm")} />
-          <span className={clsx(styles["style-font-chart"])}>
-            Usuarios activos en la plataforma
-          </span>
-        </div>
+      <div className={clsx("d-flex mb-3")}>
+        <CircleColor className={clsx("me-2", "circle-blue-sm")} />
+        <span className={clsx(styles["style-font-chart"])}>
+          Usuarios activos en la plataforma
+        </span>
+      </div>
 
-        <div
-          className={clsx(
-            "d-flex align-items-center",
-            styles["style-graph-colaboradores"]
-          )}
-        >
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              width={400}
-              height={150}
-              data={data}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-            >
-              <XAxis dataKey="name" />
-              <YAxis hide={true} />
-              <CartesianGrid stroke="#fff" strokeDashoffset="5 5" />
-              <Tooltip content={<CustomTooltip />} />
-              {/* <Legend wrapperStyle={{ fontSize: "10px" }} /> */}
-              <Line
-                type="monotone"
-                dataKey="line1"
-                name="Usuarios activos en la plataforma"
-                stroke="#fff"
-                fill="#2985e2"
-                activeDot={{ r: 8 }}
-              />
-              <Line
-                type="monotone"
-                dataKey="line2"
-                strokeDasharray="7 7"
-                name="Tiempo de actividad de usuarios en la plataforma"
-                stroke="#82ca9d"
-                fill="#82ca9d"
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
+      <div
+        className={clsx(
+          "d-flex align-items-center",
+          styles["style-graph-colaboradores"]
+        )}
+      >
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={400}
+            height={150}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis hide={true} />
+            <CartesianGrid stroke="#fff" strokeDashoffset="5 5" />
+            <Tooltip content={<CustomTooltip />} />
+            {/* <Legend wrapperStyle={{ fontSize: "10px" }} /> */}
+            <Line
+              type="monotone"
+              dataKey="line1"
+              name="Usuarios activos en la plataforma"
+              stroke="#fff"
+              fill="#2985e2"
+              activeDot={{ r: 8 }}
+            />
+            <Line
+              type="monotone"
+              dataKey="line2"
+              strokeDasharray="7 7"
+              name="Tiempo de actividad de usuarios en la plataforma"
+              stroke="#82ca9d"
+              fill="#82ca9d"
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
 
-        <div className={clsx("d-flex text-center align-item-center")}>
-          <CircleColor className={clsx("me-2", "circle-green-sm")} />
-          <span className={clsx(styles["style-font-chart"])}>
-            Tiempo de actividad de usuarios en la plataforma
-          </span>
-        </div>
+      <div className={clsx("d-flex text-center align-item-center")}>
+        <CircleColor className={clsx("me-2", "circle-green-sm")} />
+        <span className={clsx(styles["style-font-chart"])}>
+          Tiempo de actividad de usuarios en la plataforma
+        </span>
       </div>
     </div>
   );
